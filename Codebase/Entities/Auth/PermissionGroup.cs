0ab76806b0e54ada83568389fcd2ec6768071c58
jukistricker <thanhthanhbg1@gmail.com>
2026-03-25 02:13:36 +0700

    [RBAC] role, permission, permission group api

--- Codebase/Entities/Auth/PermissionGroup.cs
+++ Codebase/Entities/Auth/PermissionGroup.cs
@@ -4,4 +4,5 @@ public class PermissionGroup : BaseEntity
     public string Name { get; set; } = null!; // Tên hiển thị: "Hệ thống"
     public string Code { get; set; } = null!; // "SYSTEM_SETTING"
     public int SortOrder { get; set; }        // Sắp xếp trên UI
+    public virtual ICollection<Permission> Permissions { get; set; } = new List<Permission>();
 }
--- Codebase/Models/Dtos/Responses/RbacResponse.cs
+++ Codebase/Models/Dtos/Responses/RbacResponse.cs
@@ -8,3 +8,13 @@ public class PermissionGroupResponse:BaseResponse
     public string Code { get; set; } = null!; 
     public int SortOrder { get; set; }        
 }
+
+public class PermissionResponse:BaseResponse
+{
+    public string Name { get; set; } = null!; 
+    public string Code { get; set; } = null!; 
+    public Guid PermissionGroupId { get; set; }
+    public string PermissionGroupName { get; set; }
+    public Guid RoleId { get; set; }
+    public string RoleName { get; set; }
+}
